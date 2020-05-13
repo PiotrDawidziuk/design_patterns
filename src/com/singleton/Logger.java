daaab22f49a43df193fbd619df394d769a04fab5
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2020-05-13 21:04:41 +0200

    function prints numbers of objects and singleton function uses

--- src/com/singleton/Logger.java
+++ src/com/singleton/Logger.java
@@ -3,6 +3,16 @@ package com.singleton;
 public class Logger {
 
     private static Logger logger;
+    private static int instancesCreated = 0;
+    private static int getInstanceUsed = 0;
+
+    public static int getInstancesCreated() {
+        return instancesCreated;
+    }
+
+    public static int getGetInstanceUsed() {
+        return getInstanceUsed;
+    }
 
     private Logger(){
 
@@ -11,7 +21,9 @@ public class Logger {
     public static Logger getInstance(){
         if (logger == null){
             logger = new Logger();
+            instancesCreated++;
         }
+        getInstanceUsed++;
         return logger;
     }
 }
--- src/com/singleton/TestLogger.java
+++ src/com/singleton/TestLogger.java
@@ -4,11 +4,14 @@ public class TestLogger {
     public static void main(String[] args) {
 
         Logger obj1 = Logger.getInstance();
+        System.out.println("Hash code of obj1: "+obj1.hashCode()+" Number of Logger objects: "+Logger.getInstancesCreated());
+        System.out.println("Number of getInstsance function used: "+Logger.getGetInstanceUsed());
         Logger obj2 = Logger.getInstance();
+        System.out.println("Hash code of obj2: "+obj2.hashCode()+" Number of Logger objects: "+Logger.getInstancesCreated());
+        System.out.println("Number of getInstsance function used: "+Logger.getGetInstanceUsed());
         Logger obj3 = Logger.getInstance();
-        System.out.println(obj1.hashCode());
-        System.out.println(obj2.hashCode());
-        System.out.println(obj3.hashCode());
+        System.out.println("Hash code of obj3: "+obj3.hashCode()+" Number of Logger objects: "+Logger.getInstancesCreated());
+        System.out.println("Number of getInstsance function used: "+Logger.getGetInstanceUsed());
 
     }
 }
